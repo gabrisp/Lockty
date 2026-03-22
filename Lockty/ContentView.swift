9900b773e12115f901b66e6eedcea517ba2749fa
Gabriel - Accenture <g.sanchez.palma@accenture.com>
2026-03-22 09:58:45 +0100

    Initial Commit

---
+++ Lockty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lockty
+//
+//  Created by Gabrisp on 22/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lockty/LocktyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocktyApp.swift
+//  Lockty
+//
+//  Created by Gabrisp on 22/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LocktyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
